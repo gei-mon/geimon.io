c123a8c84c0baa6f986ac9c57e5435a10ee6959a
Kieran Fleenor <kieranfleenor@comcast.net>
2025-05-23 22:30:21 -0400

    totems js populated

--- data/Totems.js
+++ data/Totems.js
@@ -1,2 +1,21 @@
-export const tokens = [
+export const totems = [
+    {
+        "Double Double": "Turn Player draws an extra card in their Draw Phase.",
+        "Single File": "Turn Player can only attack with 1 Champion during their Battle Phase.",
+        "Fists Only": "Damage can only be dealt by attack.",
+        "You Cannot Punch Ghosts": "Damage can only be dealt by effect.",
+        "Tomb Raider Rejects": "Cards cannot be Retrieved.",
+        "Blindfold": "Cards cannot be Added.",
+        "Necromancer Died": "Cards cannot be Resurrected.",
+        "Hell Is Closed Today": "Cards cannot be Obliterated.",
+        "Glass Bones and Paper Skin": "Starting life is set at 10.",
+        "Terrifying Toddlers": "Champions with a Damage of 3 or lower deal double battle Damage.",
+        "Caught in your Underwear": "No Damage Thresholds.",
+        "All or Nothing": "The Damage a creature takes from a single event must destroy it or the Damage does nothing.",
+        "Fair Play": "Both Players play with their Hands revealed.",
+        "Pendulum of Pain": "During each End Phase, both Players take 1 Damage.",
+        "Countdown Clocktower": "At the end of the 12th turn, the Player with the highest Life wins.",
+        "Fake Doctor": "All Grant Life and Healing effects Damage the Player an equal amount instead, and all Damage and Offer effects now Grant/Heal that amount of Life.",
+        "Bluff-Fest": "All cards that can be played face-down must be before they can be activated."
+    }
 ]
