4c1c1fafdcc4e35419d991598d1259046bd2b949
Kieran Fleenor <kieranfleenor@comcast.net>
2025-06-06 09:04:49 -0400

    Pendulum of pain totem done

--- data/totems.cjs
+++ data/totems.cjs
@@ -4,17 +4,17 @@ const totems = [
         "Double Double": "Turn Player draws an extra card in their Draw Phase.", //
         "Fake Doctor": "All Grant Life and Healing effects Damage the Player an equal amount instead, and all Damage and Offer effects now Grant/Heal that amount of Life.", //
         "Countdown Clocktower": "At the end of the 12th turn, the Player with the highest Life wins.", //
-        "Single File": "Turn Player can only attack with 1 Champion during their Battle Phase.",
-        "Fists Only": "Damage can only be dealt by attack.",
+        "Pendulum of Pain": "During each End Phase, both Players take 1 Damage.", //
         "Tomb Raider Rejects": "Cards cannot be Retrieved.",
-        "Blindfolded": "Cards cannot be Added.",
         "Necromancer Died": "Cards cannot be Resurrected.",
-        "Terrifying Toddlers": "Champions with a Damage of 3 or lower deal double battle Damage.",
+        "Blindfolded": "Cards cannot be Added.",
         "Caught in your Underwear": "No Damage Thresholds.",
+        "Shared Interest": "Neither Player can deal damage to any other until the start of Turn 7.",
+        "Single File": "Turn Player can only attack with 1 Champion during their Battle Phase.",
+        "Terrifying Toddlers": "Champions with a Damage of 3 or lower deal double battle Damage.",
+        "Fists Only": "Damage can only be dealt by attack.",
         "All or Nothing": "The Damage a creature takes from a single event must Destroy it or the Damage does nothing.",
-        "Fair Play": "Both Players play with their Hands revealed.",
-        "Pendulum of Pain": "During each End Phase, both Players take 1 Damage.",
-        "Shared Interest": "Neither Player can deal damage to any other until the start of Turn 7."
+        "Fair Play": "Both Players play with their Hands revealed."
     }
 ]
 
--- data/totems.js
+++ data/totems.js
@@ -1,19 +1,19 @@
 export const totems = [
     {
-        "Double Double": "Turn Player draws an extra card in their Draw Phase.",
-        "Countdown Clocktower": "At the end of the 12th turn, the Player with the highest Life wins.",
-        "Single File": "Turn Player can only attack with 1 Champion during their Battle Phase.",
-        "Fists Only": "Damage can only be dealt by attack.",
+        "Glass Bones and Paper Skin": "Starting life is set at 10.", //
+        "Double Double": "Turn Player draws an extra card in their Draw Phase.", //
+        "Fake Doctor": "All Grant Life and Healing effects Damage the Player an equal amount instead, and all Damage and Offer effects now Grant/Heal that amount of Life.", //
+        "Countdown Clocktower": "At the end of the 12th turn, the Player with the highest Life wins.", //
+        "Pendulum of Pain": "During each End Phase, both Players take 1 Damage.", //
         "Tomb Raider Rejects": "Cards cannot be Retrieved.",
-        "Blindfolded": "Cards cannot be Added.",
         "Necromancer Died": "Cards cannot be Resurrected.",
-        "Glass Bones and Paper Skin": "Starting life is set at 10.",
-        "Terrifying Toddlers": "Champions with a Damage of 3 or lower deal double battle Damage.",
+        "Blindfolded": "Cards cannot be Added.",
         "Caught in your Underwear": "No Damage Thresholds.",
-        "All or Nothing": "The Damage a creature takes from a single event must destroy it or the Damage does nothing.",
-        "Fair Play": "Both Players play with their Hands revealed.",
-        "Pendulum of Pain": "During each End Phase, both Players take 1 Damage.",
-        "Fake Doctor": "All Grant Life and Healing effects Damage the Player an equal amount instead, and all Damage and Offer effects now Grant/Heal that amount of Life.",
-        "Shared Interest": "Neither Player can deal damage to any other until the start of Turn 7."
+        "Shared Interest": "Neither Player can deal damage to any other until the start of Turn 7.",
+        "Single File": "Turn Player can only attack with 1 Champion during their Battle Phase.",
+        "Terrifying Toddlers": "Champions with a Damage of 3 or lower deal double battle Damage.",
+        "Fists Only": "Damage can only be dealt by attack.",
+        "All or Nothing": "The Damage a creature takes from a single event must Destroy it or the Damage does nothing.",
+        "Fair Play": "Both Players play with their Hands revealed."
     }
 ]
