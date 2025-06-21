9572bd4214a2a0c8ced4630f8492ba5232f08477
Kieran Fleenor <kieranfleenor@comcast.net>
2025-06-21 17:42:12 -0400

    life change done

--- data/totems.cjs
+++ data/totems.cjs
@@ -1,20 +1,20 @@
 const totems = [
     {
-        "Glass Bones and Paper Skin": "Starting life is set at 10.", //
-        "Double Double": "Turn Player draws an extra card in their Draw Phase.", //
-        "Fake Doctor": "All Grant Life and Healing effects Damage the Player an equal amount instead, and all Damage and Offer effects now Grant/Heal that amount of Life.", //
-        "Countdown Clocktower": "At the end of the 12th turn, the Player with the highest Life wins.", //
-        "Pendulum of Pain": "During each End Phase, both Players take 1 Damage.", //
-        "Tomb Raider Rejects": "Cards cannot be Retrieved.", //
-        "Necromancer Died": "Cards cannot be Resurrected.", //
-        "Blindfolded": "Cards cannot be Added.", //
-        "Single File": "Turn Player can only attack with 1 Champion during their Battle Phase.", //
-        "Fair Play": "Both Players play with their Hands revealed.", //
-        "Shared Interest": "Neither Player can deal damage to any other until the start of Turn 7.",
-        "Terrifying Toddlers": "Champions with a Damage of 3 or lower deal double battle Damage.",
-        "Fists Only": "Damage can only be dealt by attack.",
-        "Caught in your Underwear": "No Damage Thresholds.",
-        "All or Nothing": "The Damage a Champion takes from a single event must Destroy it or the Damage does nothing."
+        "Glass Bones and Paper Skin": "Starting life is set at 10.", // Server
+        "Double Double": "Turn Player draws an extra card in their Draw Phase.", // Player
+        "Fake Doctor": "All Grant Life and Healing effects Damage the Player an equal amount instead, and all Damage and Offer effects now Grant/Heal that amount of Life.", // Player
+        "Countdown Clocktower": "At the end of the 12th turn, the Player with the highest Life wins.", // Player
+        "Pendulum of Pain": "During each End Phase, both Players take 1 Damage.", // Player
+        "Tomb Raider Rejects": "Cards cannot be Retrieved.", // Player
+        "Necromancer Died": "Cards cannot be Resurrected.", // Player
+        "Blindfolded": "Cards cannot be Added.", // Player
+        "Single File": "Turn Player can only attack with 1 Champion during their Battle Phase.", // Player
+        "Fair Play": "Both Players play with their Hands revealed.", // Server (kinda done)
+        "Shared Interest": "Neither Player can deal damage to any other until the start of Turn 7.", // Player
+        "Terrifying Toddlers": "Champions with a Damage of 3 or lower deal double battle Damage.", // Player
+        "Fists Only": "Damage can only be dealt by attack.", // Player
+        "Caught in your Underwear": "No Damage Thresholds.", // Player
+        "All or Nothing": "The Damage a Champion takes from a single event must Destroy it or the Damage does nothing." // Player
     }
 ]
 
