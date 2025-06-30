330fbfc354396999e0d54fd52e78bd272dadb3c3
Kieran Fleenor <kieranfleenor@comcast.net>
2025-06-30 10:07:42 -0400

    Totem text adjustment

--- data/totems.js
+++ data/totems.js
@@ -2,7 +2,7 @@ export const totems = [
     {
         "Glass Bones and Paper Skin": "Starting life is set at 10.", //
         "Double Double": "Turn Player draws an extra card in their Draw Phase.", //
-        "Fake Doctor": "All Grant Life and Healing effects Damage the Player an equal amount instead, and all Damage and Offer effects now Grant/Heal that amount of Life.", //
+        "Fake Doctor": "All Grant Life and Healing effects Damage the Player an equal amount instead, and all Damage and Offer effects now Grant/Heal that amount of Life to the Player.", //
         "Countdown Clocktower": "At the end of the 12th turn, the Player with the highest Life wins.", //
         "Pendulum of Pain": "During each End Phase, both Players take 1 Damage.", //
         "Tomb Raider Rejects": "Cards cannot be Retrieved.",
