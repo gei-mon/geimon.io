4c4c4540f1d6363dea0bf529124ab58ff9f565a0
Kieran Fleenor <kieranfleenor@comcast.net>
2025-07-16 11:14:11 -0400

    hover top cards from tombs and voids implemented, style updated for showing active lobbies

--- data/totems.cjs
+++ data/totems.cjs
@@ -2,7 +2,7 @@ const totems = [
     {
         "Glass Bones and Paper Skin": "Starting life is set at 10.", // Server
         "Double Double": "Turn Player draws an extra card in their Draw Phase.", // Player
-        "Fake Doctor": "All Grant Life and Healing effects Damage the Player an equal amount instead, and all Damage and Offer effects now Grant/Heal that amount of Life to the Player.", // Player
+        "Fake Doctor": "All Gain Life effects aimed towards a Player now Damage the Player that amount instead, and all Damage and Offer effects now Grant that amount of Life instead.", // Server
         "Countdown Clocktower": "At the end of the 12th turn, the Player with the highest Life wins.", // Server
         "Pendulum of Pain": "During each End Phase, both Players take 1 Damage.", // Player
         "Tomb Raider Rejects": "Cards cannot be Retrieved.", // Player
@@ -11,7 +11,7 @@ const totems = [
         "Single File": "Turn Player can only attack with 1 Champion during their Battle Phase.", // Player
         "Fair Play": "Both Players play with their Hands revealed.", // Server (kinda done)
         "Shared Interest": "Neither Player can deal damage to any other until the start of Turn 7.", // Player
-        "Terrifying Toddlers": "Champions with a Damage of 3 or lower deal double battle Damage.", // Player
+        "Terrifying Toddlers": "Champions with a Damage of 2 or lower deal double battle Damage.", // Player
         "Fists Only": "Damage can only be dealt by attack.", // Player
         "Caught in your Underwear": "No Damage Thresholds.", // Player
         "All or Nothing": "The Damage a Champion takes from a single event must Destroy it or the Damage does nothing." // Player
--- data/totems.js
+++ data/totems.js
@@ -2,7 +2,7 @@ export const totems = [
     {
         "Glass Bones and Paper Skin": "Starting life is set at 10.", //
         "Double Double": "Turn Player draws an extra card in their Draw Phase.", //
-        "Fake Doctor": "All Grant Life and Healing effects Damage the Player an equal amount instead, and all Damage and Offer effects now Grant/Heal that amount of Life to the Player.", //
+        "Fake Doctor": "All Gain Life effects aimed towards a Player now Damage the Player that amount instead, and all Damage and Offer effects now Grant that amount of Life instead.", //
         "Countdown Clocktower": "At the end of the 12th turn, the Player with the highest Life wins.", //
         "Pendulum of Pain": "During each End Phase, both Players take 1 Damage.", //
         "Tomb Raider Rejects": "Cards cannot be Retrieved.",
@@ -11,7 +11,7 @@ export const totems = [
         "Caught in your Underwear": "No Damage Thresholds.",
         "Shared Interest": "Neither Player can deal damage to any other until the start of Turn 7.",
         "Single File": "Turn Player can only attack with 1 Champion during their Battle Phase.",
-        "Terrifying Toddlers": "Champions with a Damage of 3 or lower deal double battle Damage.",
+        "Terrifying Toddlers": "Champions with a Damage of 2 or lower deal double battle Damage.",
         "Fists Only": "Damage can only be dealt by attack.",
         "All or Nothing": "The Damage a Champion takes from a single event must Destroy it or the Damage does nothing.",
         "Fair Play": "Both Players play with their Hands revealed."
