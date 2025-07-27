200f4c1187a4e21b24cbb1176afb298cfddeb59a
Kieran Fleenor <kieranfleenor@comcast.net>
2025-07-27 06:29:54 -0400

    Updated front page backend for better changelog tracking

---
+++ data/issues.js
@@ -0,0 +1,22 @@
+const issueData = [
+    {
+        title: "You There!",
+        text: "Email 'geimon.help@gmail.com' to submit bugs as well as request features or recommend cards to add!"
+    },
+    {
+        title: "Known Bug",
+        text: "All cards within games are targeted by ID, not UUID, which means that clicking to target, attack, or block with 1 card mirrors the action for all cards of the same kind. Issue is only really noticeable with Tokens as all other cards are 1 per Deck."
+    },
+    {
+        title: "Known Bug",
+        text: "Facedown Arsenal cards do not hover perfectly. Temporary fix implemented: Just hover the space below them to see which one is which."
+    },
+    {
+        title: "Known Bug",
+        text: "Attachments are kinda wonky. Temporary fix implemented: Use the Clear Attachments button to remove all on screen and then you can redraw them."
+    },
+    {
+        title: "Known Bug",
+        text: "Cards do not go to the correct (owners) Deck, Tomb, and Void, only to the player's that clicks the button, so some shimmying has to be done."
+    }
+];
---
+++ data/news.js
@@ -0,0 +1,7 @@
+
+const newsData = [
+    {
+        date: "July 2025",
+        text: "v1.1 Alpha Released! <br> With significant quality of life features such as Mill X Cards and a better formatting method for Champions, we hope you enjoy the latest improvements to Geimon!"
+    }
+];
